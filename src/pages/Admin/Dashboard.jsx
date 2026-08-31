fafdeb5e4e90fb2d4a84a22d98e41fd27b87cc93
ibrahim <moussaibrahimcoulibaly2@gmail.com>
2026-08-31 03:00:16 +0000

    feat: créer le Dashboard admin

---
+++ src/pages/Admin/Dashboard.jsx
@@ -0,0 +1,13 @@
+function Dashboard() {
+  return (
+    <div className="container mx-auto p-8">
+      <h1 className="text-3xl font-bold mb-4">Tableau de bord</h1>
+
+      <p className="text-gray-600">
+        Bienvenue dans l'administration de KiffStyle.
+      </p>
+    </div>
+  );
+}
+
+export default Dashboard;
